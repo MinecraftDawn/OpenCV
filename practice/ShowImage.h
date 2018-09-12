279159ea9b18f81b08192f9dff792ee376b20a7c
MinecraftDawn <eric4902077@gmail.com>
2018-09-12 20:49:24 +0800

    Gray scale

--- practice/ShowImage.h
+++ practice/ShowImage.h
@@ -14,15 +14,13 @@ using namespace std;
 
 
 class ShowImage {
-private:
-    char ig[] = "B:\\血小板.jpg";
-    Mat image;
-
 public:
+
     void showImg(){
+        char ig[] = "B:\\血小板.jpg";
 
         // 載入圖檔
-        image = imread(ig, CV_LOAD_IMAGE_COLOR);
+        Mat image = imread(ig, CV_LOAD_IMAGE_COLOR);
 
         // 檢查讀檔是否成功
         if (!image.data)
@@ -41,6 +39,30 @@ public:
         imshow("Display window", image);
 
         // 視窗等待按鍵
+        waitKey(0);
+    }
+
+    void showImg2(){
+        char ig[] = "B:\\血小板.jpg";
+
+        // 載入圖檔
+        Mat image = imread(ig, CV_LOAD_IMAGE_COLOR);
+
+        imshow("2",image);
+        waitKey(0);
+    }
+
+    void showImg3(){
+        char ig[] = "B:\\血小板.jpg";
+
+        Mat testColor = imread(ig,CV_LOAD_IMAGE_GRAYSCALE);
+
+        namedWindow("Display window", CV_WINDOW_NORMAL);
+
+        imshow("Display window",testColor);
+
+        imwrite("B:\\血小板灰.jpg",testColor);
+
         waitKey(0);
     }
 
