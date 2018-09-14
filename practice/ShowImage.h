44f63c7c57a2024cc15ae1e57cb96d1c856765bc
MinecraftDawn <eric4902077@gmail.com>
2018-09-14 22:36:32 +0800

    test

--- practice/ShowImage.h
+++ practice/ShowImage.h
@@ -84,14 +84,14 @@ public:
         waitKey(0);
     }
 
-    void showImg5(){ //¦Û­q¦Ç¶¥
+    void showImg5() { //¦Û­q¦Ç¶¥
         char ig[] = "B:\\¦å¤pªO.jpg";
 
         Mat file = imread(ig, CV_LOAD_IMAGE_GRAYSCALE);
 
         for (int i = 0; i < file.rows; ++i) {
             for (int j = 0; j < file.cols; ++j) {
-                file.at<uint8_t >(i,j) = file.at<uint8_t >(i,j) *0.8;
+                file.at<uint8_t>(i, j) = file.at<uint8_t>(i, j) * 0.8;
             }
         }
 
@@ -119,30 +119,71 @@ public:
 
     }
 
-    void showImg7(){
+    void showImg7() {
         char ig[] = "B:\\¦å¤pªO.jpg";
 
         Mat file = imread(ig, CV_LOAD_IMAGE_COLOR);
 
         Mat splitC[3];
 
-        split(file,splitC);
+        split(file, splitC);
 
-        imshow("B",splitC[0]);
-        imshow("G",splitC[1]);
-        imshow("R",splitC[2]);
+        imshow("B", splitC[0]);
+        imshow("G", splitC[1]);
+        imshow("R", splitC[2]);
 
-        splitC[0] = Mat::zeros(splitC[0].size(),CV_8UC1);
+        splitC[0] = Mat::zeros(splitC[0].size(), CV_8UC1);
 //        splitC[1] = Mat::zeros(splitC[1].size(),CV_8UC1);
-        splitC[2] = Mat::zeros(splitC[2].size(),CV_8UC1);
+        splitC[2] = Mat::zeros(splitC[2].size(), CV_8UC1);
 
         Mat output;
 
-        merge(splitC,3,output);
+        merge(splitC, 3, output);
 
-        imshow("Merged",output);
+        imshow("Merged", output);
 
         waitKey(0);
+    }
+
+    void showImg8() {
+        char ig[] = "B:\\¦å¤pªO.jpg";
+
+        Mat file = imread(ig, CV_LOAD_IMAGE_COLOR);
+
+        Mat originalFloat;
+
+        file.convertTo(originalFloat, CV_32FC1, 1.0 / 255.0);
+
+        Mat dftOriginal;
+
+        Mat originalComplex[2] = {originalFloat,Mat::zeros(originalComplex->size(),CV_32F)};
+
+        Mat dftReady;
+
+        merge(originalComplex,2,dftReady);
+
+        dft(dftReady,dftOriginal,DFT_COMPLEX_OUTPUT);
+
+        Mat splitArray[2] = {Mat::zeros(dftOriginal.size(),CV_32F),Mat::zeros(dftOriginal.size(),CV_32F)};
+
+        split(dftOriginal,splitArray);
+
+        Mat dftMagnitude;
+
+        magnitude(splitArray[0],splitArray[1],dftMagnitude);
+
+        dftMagnitude += Scalar::all(1);
+
+        log(dftMagnitude,dftMagnitude);
+
+        normalize(dftMagnitude,dftMagnitude,0,1,CV_MINMAX);
+
+        imshow("DFT",dftMagnitude);
+
+        waitKey(0);
+
+
+
     }
 
 };
